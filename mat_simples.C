69a32c4a7f1e129c567ef0f404e38696e5695a9d
samuel.gomes.124 <samuel.avelar@estudante.iftm.edu.br>
2026-07-17 23:48:53 -0300

    Refactor matrix input and output code

--- mat_simples.C
+++ mat_simples.C
@@ -1,19 +1,18 @@
 int main()
 {
-  int mat[5][5];
-int i,j;
-     for(i=0 ; i<5 ; i++){
-            
+    int mat[5][5];
+    int i,j;
+       for(i=0 ; i<5 ; i++){    
              for(j=0 ; j<5 ; j++){
             printf(" QUAL NUMERO DESEJA ACESSAR[%d][%d]", i,j);
                  scanf("%d",&mat [i][j]);
              }  
                                             
-     }
-        for(i=0 ; i<5 ; i++){
+       }
+          for(i=0 ; i<5 ; i++){
             
              for(j=0 ; j<5 ; j++){
-            printf("%d",mat [i][j]);
+              printf("%d",mat [i][j]);
             
        
              }
