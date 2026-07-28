5018b1cf08a7bb7a2e238571671378d6d0b884ec
samuel.gomes.124 <samuel.avelar@estudante.iftm.edu.br>
2026-07-28 11:21:09 -0300

    Create beecrowd.c

---
+++ beecrowd.c
@@ -0,0 +1,12 @@
+int main(){
+     double  area ,n =3.14159;
+      double raio;
+       
+       scanf("%lf",&raio);
+     
+       area=pow(raio,2)*n;
+           
+         printf("a= %.4lf\n",area);
+      
+      return 0;
+  }
