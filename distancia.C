4825db512d3e475ef963b3fb8e469e620d98f182
samuel.gomes.124 <samuel.avelar@estudante.iftm.edu.br>
2026-08-10 14:39:47 -0300

    Create distancia.C

---
+++ distancia.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+        int DISTANCIA,tempo;
+
+         scanf("%d", &DISTANCIA);
+             tempo = DISTANCIA*2;
+            printf("%d MINUTOS\n", tempo);
+    return 0;
+}
