4dee207ab0329160e62f187b44c2404f30a111d2
samuel.gomes.124 <samuel.avelar@estudante.iftm.edu.br>
2026-08-12 14:00:37 -0300

    Create raiz.c

---
+++ raiz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h> 
+int main() {
+   float x1 , x2 ,y1,y2;
+   double distancia;
+     scanf("%f %f", &x1, &y1);
+     scanf("%f %f", &x2, &y2);
+    distancia = sqrt(pow (x2 - x1,2)+ pow (y2 - y1 , 2));
+    printf("%.4lf\n", distancia);
+    return 0;
+}
