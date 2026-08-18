70a37a95fd95cfbcd6acf2fb69b6d7d4338fbd42
samuel.gomes.124 <samuel.avelar@estudante.iftm.edu.br>
2026-08-17 23:09:14 -0300

    Refactor distance calculation for two points
    Updated the code to include a second point for distance calculation.

--- .c
+++ .c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-#include< math.h>
+  #include <stdio.h>
+#include   <math.h>
 int main() {
-    double x1 , x2 , y1, y2 , dist;
-   scanf("%lf %lf " , &x1 , &y1);
-   
-   
-   
+        double x1 , y1  , x2, y2 , dist;
+       scanf("%lf %lf " , &x1 , &y1);
+       scanf("%lf %lf " , &x2 , &y2);
+       dist = sqrt(pow(x2-x1 , 2) + pow(y2 - y1,2));
+        printf("%.4lf",dist);
+
     return 0;
 }
