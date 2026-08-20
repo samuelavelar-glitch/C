98caf7057a3b051e3696aacd6513047210d80834
samuel.gomes.124 <samuel.avelar@estudante.iftm.edu.br>
2026-08-20 13:10:43 -0300

    Update valor de pecas.c

--- valor de pecas.c
+++ valor de pecas.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
- 
-int main() {
- 
-    int codigo_peca_1 , codigo_peca_2 ,numero_de_peças1,numero_de_peças2;
-    float valor_unitario1 , valor_unitario2 , valor_1 ,valor_2 , total;
-    
-        scanf("%d %d %f", &codigo_peca_1,&numero_de_peças1,&valor_unitario1);
-        
-            
-            valor_1 = (numero_de_peças1*valor_unitario1);
-        
-          scanf("%d %d %f", &codigo_peca_2,&numero_de_peças2,&valor_unitario2);
-            
-                  valor_2 = (numero_de_peças2*valor_unitario2);
-                total = valor_1+valor_2;
-            
-                    
-                      
 
-            printf("valor A Pagar = % .2f\n " , total);
+int main() {
+    int codigo_peca1, codigo_peca2;
+    int numero_de_pecas1, numero_de_pecas2;
+
+    float valor_unitario1, valor_unitario2;
+    float valor_1, valor_2, total;
+
+        scanf("%d %d",&codigo_peca1,&numero_de_pecas1);
+          scanf("%f",&valor_unitario1);
+
+            valor_1 = numero_de_pecas1*valor_unitario1;
+
+          scanf("%d %d", &codigo_peca2,&numero_de_pecas2);
+            scanf("%f",&valor_unitario2);
+                  valor_2 = numero_de_pecas2*valor_unitario2;
+                total = valor_1+valor_2;
+              printf("VALOR A PAGAR: R$ %.2f\n", total);
     return 0;
 }
