efe29b214a32918288479a861c30bafba1bc34c1
samuel.gomes.124 <samuel.avelar@estudante.iftm.edu.br>
2026-08-26 13:25:10 -0300

    Create triangulos.c

---
+++ triangulos.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<math.h> 
+int main() {
+ 
+double a , b ,c ,temp ;
+ if( scanf("%lf %lf %lf ", &a , &b , &c) != 3) return 0;
+  
+    if (a < b) { temp = a; a = b; b = temp; }
+     if (a < c) { temp = a; a = c; c = temp; }
+     if (b < c) { temp = b; b = c; c = temp; }
+   
+    if(a >= b+c){
+      printf("NAO FORMA TRIANGULO\n");
+      return 0;    
+  }
+    if(a * a == b * b + c * c){
+      printf("TRIANGULO RETANGULO\n");
+      
+  }
+    if(a * a > b * b + c * c){
+      printf("TRIANGULO OBTUSANGULO\n");
+      
+  }
+  
+   if(a * a < b * b + c * c){
+      printf("TRIANGULO ACUTANGULO\n");
+      
+  }
+     if(a == b && b == c){
+      printf("TRIANGULO EQUILATERO\n");
+      
+  }
+         else if (a == b || b == c || a == c) {
+        printf("TRIANGULO ISOSCELES\n");
+        
+    }
+      
+  
+  
+    return 0;
+}
