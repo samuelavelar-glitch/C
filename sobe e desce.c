df484b7edad19b405e97a0698d3ed26f82c7ee94
samuel.gomes.124 <samuel.avelar@estudante.iftm.edu.br>
2026-09-22 00:37:37 -0300

    Implement main function to compare two integers

---
+++ sobe e desce.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int  x , y; 
+     
+     while (x !=y){
+         scanf("%d %d", &x , &y);
+          if(x < y){
+              printf("Crescente\n");
+          }
+          else if (x > y){
+          printf("Decrescente\n"); 
+          }
+     }
+    return 0;
+}
